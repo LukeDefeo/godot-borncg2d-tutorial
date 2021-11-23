2a3ca35f440748db167c54807a5cb04aa5a838e3
luke.defeo <luke.defeo@justeattakeaway.com>
2021-11-23 12:05:20 +0000

    initial

---
+++ Player.gd
@@ -0,0 +1,56 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+const SPEED = 250
+const GRAVITY = 30
+const JUMP_FORCE = 900
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+var velocity = Vector2(0,0)
+
+func _physics_process(delta):
+	if Input.is_action_pressed("left"):
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = true
+		velocity.x = -SPEED
+	elif Input.is_action_pressed("right"):
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = false
+		velocity.x = SPEED
+	else:
+		$AnimatedSprite.play("idle")
+		
+	if not is_on_floor():
+		$AnimatedSprite.play("air")
+	# is on floor works bc we set up dir in move and slide
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -JUMP_FORCE
+	# first move the char
+	
+	velocity.y = velocity.y + GRAVITY
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	# then interp velocity back to 0 to slow down, if the char is press again goes back to
+	# full speed
+	velocity.x = lerp(velocity.x,0,0.2)
+	
+
+#func _process(delta):
+#	pass
+
+
+func _on_Fallzone_body_entered(body):
+	get_tree().change_scene("res://level1.tscn")
+
+
+var coins_collected = 0
+func add_coin():
+	coins_collected += 1
+	if coins_collected == 2:
+		get_tree().change_scene("res://level1.tscn")
---
+++ level1.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+var start
+var end
+
+func _process(delta):
+	pass
+#	if (Input.is_action_just_pressed("touch")):		
+#		start = get_global_mouse_position()
+		
+#	if (Input.is_action_just_released("touch")):
+#		end = get_global_mouse_position()
+	
+	
+#	var dir = (end - start).normalized()
+		
+	
